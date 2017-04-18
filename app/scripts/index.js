7770f7c80d836c9b54f95f0e0fb027d2c1203777
John Baldwin III <john.baldwin.jb3@gmail.com>
2017-04-18 18:40:31 -0400

    updating portfolio project

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,14 +1,32 @@
-var $ = require('jquery');
 
+var $ = window.jQuery = require('jquery');
+var Materialize = require('materialize-css');
+require('../node_modules/materialize-css/js/sideNav.js');
+require('../../node_modules/materialize-css/js/parallax.js');
+require('../../node_modules/materialize-css/js/carousel.js');
 
-$(document).ready(function(){
+(function($){
+
   $('.carousel').carousel();
-});
-// Next slide
-$('.carousel').carousel('next');
-$('.carousel').carousel('next', 3); // Move next n times.
-// Previous slide
-$('.carousel').carousel('prev');
-$('.carousel').carousel('prev', 4); // Move prev n times.
-// Set to nth slide
-$('.carousel').carousel('set', 4);
+
+  // Next slide
+  $('.carousel').carousel('next');
+  $('.carousel').carousel('next', 3); // Move next n times.
+  // Previous slide
+  $('.carousel').carousel('prev');
+  $('.carousel').carousel('prev', 4); // Move prev n times.
+  // Set to nth slide
+  $('.carousel').carousel('set', 4);
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+    $(document).on('click', '.nav-wrapper a', function(e){
+      e.preventDefault();
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 1000);
+    });
+
+   // end of document ready
+})(jQuery);
